91cccfd6334b32d0cef049808d4a3fecabfed66d
unknown <cooperslempy@gmail.com>
2024-12-27 03:56:20 +0300

    Исправлено

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -4,6 +4,7 @@ import typing as tp
 
 def is_prime(n: int) -> bool:
     """
+    Tests to see if a number is prime.
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -11,78 +12,96 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    if n <= 1:
+    if n < 0 and n != 1:
         return False
-    if n <= 3:
-        return True  # 2 и 3 - простые
-    if n % 2 == 0 or n % 3 == 0:
-        return False
+    res_1 = 0
+    res_2 = False
+    if n == 2:
+        return True
+    else:
+        for i in range(2, n):
 
-    # Проверяем делители вида 6k ± 1
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
+            res_1 = n % i
+            if res_1 == 0:
+                return False
+                break
+            else:
+                return True
 
-    return True
+    return res_2
+
+
+print(is_prime(2))
 
 
 def gcd(a: int, b: int) -> int:
     """
+    Euclid's algorithm for determining the greatest common divisor.
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
     1
     """
-    while b != 0:
-        a, b = b, a % b
-    return a
+    while a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        else:
+            b = b % a
+
+    return max(a, b)
+
+
+print(gcd(3, 7))
+
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
     >>> multiplicative_inverse(7, 40)
     23
     """
-    def egcd(a, b):
-        if a == 0:
-            return (b, 0, 1)
-        g, y, x = egcd(b % a, a)
-        return (g, x - (b // a) * y, y)
+    a, b = e, phi
+    x0, x1, y0, y1 = 1, 0, 0, 1
 
-    g, x, y = egcd(e, phi)
-    if g != 1:
-        # Обратный элемент не существует, e и phi не взаимно просты
-        return None
-    else:
-        return x % phi
+    while b != 0:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1, x0 - q * x1
+        y0, y1 = y1, y0 - q * y1
+    return x0 % phi
+
+
+print(multiplicative_inverse(7, 40))
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
-        raise ValueError('Both numbers must be prime.')
+        raise ValueError("Both numbers must be prime.")
     elif p == q:
-        raise ValueError('p and q cannot be equal')
+        raise ValueError("p and q cannot be equal")
 
-    # n = p * q
     n = p * q
+    # PUT YOUR CODE HERE
 
-    # phi = (p-1)*(q-1)
     phi = (p - 1) * (q - 1)
+    # PUT YOUR CODE HERE
 
+    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
+    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
 
-    # Публичный ключ: (e, n)
-    # Приватный ключ: (d, n)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
-
-
 
 
 def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
@@ -99,7 +118,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
@@ -118,3 +137,4 @@ if __name__ == "__main__":
     print("Decrypting message with public key ", public, " . . .")
     print("Your message is:")
     print(decrypt(public, encrypted_msg))
+    
